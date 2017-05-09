7179e16d86490c7984b26ae7c9a55e2c9081e846
Felipe Okino <felipe1okino@live.com>
2017-05-09 20:41:57 -0300

    teste

---
+++ ControleEstacionamento/Modelos/FuncionarioModelo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleEstacionamento.Modelos
+{
+    public class FuncionarioModelo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public long Cpf { get; set; }
+        public long Telefone { get; set; }
+        public long Celular { get; set; }
+        public decimal Salario { get; set; }
+
+        public FuncionarioModelo()
+        {
+            
+        }
+
+    }
+}
