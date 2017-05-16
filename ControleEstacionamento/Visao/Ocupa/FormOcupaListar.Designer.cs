3398e15bdbf9acf0164d75f1d399fc7273edda5f
tiagoitera <tiago@itera.com.br>
2017-05-16 14:42:36 -0300

    Form Ocupa

---
+++ ControleEstacionamento/Visao/Ocupa/FormOcupaListar.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ControleEstacionamento.Visao.Ocupa
+{
+    partial class FormOcupaListar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdicionarVeiculo = new System.Windows.Forms.Button();
+            this.btnDetalhesVeiculo = new System.Windows.Forms.Button();
+            this.btnEditarVeiculo = new System.Windows.Forms.Button();
+            this.btnExcluirVeiculo = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnFecharVeiculo = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAdicionarVeiculo
+            // 
+            this.btnAdicionarVeiculo.Location = new System.Drawing.Point(12, 12);
+            this.btnAdicionarVeiculo.Name = "btnAdicionarVeiculo";
+            this.btnAdicionarVeiculo.Size = new System.Drawing.Size(75, 23);
+            this.btnAdicionarVeiculo.TabIndex = 2;
+            this.btnAdicionarVeiculo.Text = "Adicionar";
+            this.btnAdicionarVeiculo.UseVisualStyleBackColor = true;
+            this.btnAdicionarVeiculo.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // btnDetalhesVeiculo
+            // 
+            this.btnDetalhesVeiculo.Location = new System.Drawing.Point(93, 12);
+            this.btnDetalhesVeiculo.Name = "btnDetalhesVeiculo";
+            this.btnDetalhesVeiculo.Size = new System.Drawing.Size(75, 23);
+            this.btnDetalhesVeiculo.TabIndex = 3;
+            this.btnDetalhesVeiculo.Text = "Detalhes";
+            this.btnDetalhesVeiculo.UseVisualStyleBackColor = true;
+            this.btnDetalhesVeiculo.Click += new System.EventHandler(this.btnDetalhesVeiculo_Click);
+            // 
+            // btnEditarVeiculo
+            // 
+            this.btnEditarVeiculo.Location = new System.Drawing.Point(174, 12);
+            this.btnEditarVeiculo.Name = "btnEditarVeiculo";
+            this.btnEditarVeiculo.Size = new System.Drawing.Size(75, 23);
+            this.btnEditarVeiculo.TabIndex = 4;
+            this.btnEditarVeiculo.Text = "Editar";
+            this.btnEditarVeiculo.UseVisualStyleBackColor = true;
+            this.btnEditarVeiculo.Click += new System.EventHandler(this.btnEditarVeiculo_Click);
+            // 
+            // btnExcluirVeiculo
+            // 
+            this.btnExcluirVeiculo.Location = new System.Drawing.Point(255, 12);
+            this.btnExcluirVeiculo.Name = "btnExcluirVeiculo";
+            this.btnExcluirVeiculo.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluirVeiculo.TabIndex = 5;
+            this.btnExcluirVeiculo.Text = "Excluir";
+            this.btnExcluirVeiculo.UseVisualStyleBackColor = true;
+            this.btnExcluirVeiculo.Click += new System.EventHandler(this.btnExcluirVeiculo_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.listView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 60);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(867, 432);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnFecharVeiculo);
+            this.panel2.Controls.Add(this.btnAdicionarVeiculo);
+            this.panel2.Controls.Add(this.btnDetalhesVeiculo);
+            this.panel2.Controls.Add(this.btnExcluirVeiculo);
+            this.panel2.Controls.Add(this.btnEditarVeiculo);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 444);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(867, 48);
+            this.panel2.TabIndex = 8;
+            // 
+            // btnFecharVeiculo
+            // 
+            this.btnFecharVeiculo.Location = new System.Drawing.Point(780, 12);
+            this.btnFecharVeiculo.Name = "btnFecharVeiculo";
+            this.btnFecharVeiculo.Size = new System.Drawing.Size(75, 23);
+            this.btnFecharVeiculo.TabIndex = 6;
+            this.btnFecharVeiculo.Text = "Fechar";
+            this.btnFecharVeiculo.UseVisualStyleBackColor = true;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(867, 60);
+            this.panel3.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(867, 432);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // FormOcupaListar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 492);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel3);
+            this.Name = "FormOcupaListar";
+            this.Text = "Lista de Vagas";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnAdicionarVeiculo;
+        private System.Windows.Forms.Button btnDetalhesVeiculo;
+        private System.Windows.Forms.Button btnEditarVeiculo;
+        private System.Windows.Forms.Button btnExcluirVeiculo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnFecharVeiculo;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
---
+++ ControleEstacionamento/Visao/Ocupa/FormOcupaListar.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleEstacionamento.Visao.Ocupa
+{
+    public partial class FormOcupaListar : Form
+    {
+        //teste
+        BindingList<Modelos.VagaModelo> list = new BindingList<Modelos.VagaModelo>();
+
+        public FormOcupaListar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnDetalhesVeiculo_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btnEditarVeiculo_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnExcluirVeiculo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Deseja excluir?", "Confirmação", MessageBoxButtons.YesNo);
+        }
+    }
+}
