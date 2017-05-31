3ed6856ff5ecc42729dd766b6056afadebe0f065
tiago.spana <tiago.spana@hotmail.com>
2017-05-31 08:46:12 -0300

    Criação do DAO

---
+++ ControleEstacionamento/DAO/Conexao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace ControleEstacionamento.DAO
+{
+    internal class Conexao
+    {
+        private static Conexao current;
+        MySqlConnection Connection;
+        private Conexao()
+        {
+
+            Connection = new MySqlConnection();
+        }
+        public static Conexao Current
+        {
+            get
+            {
+                if (current == null)
+                    current = new Conexao();
+                return current;
+            }
+        }
+
+
+    }
+}
