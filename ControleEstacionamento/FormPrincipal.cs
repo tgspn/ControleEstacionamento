1e76f6eb289f41b22d77f8b379dd5fed86b2cf0d
tiagoitera <tiago@itera.com.br>
2017-06-08 18:18:32 -0300

    Criação do controler de veiculo

--- ControleEstacionamento/FormPrincipal.cs
+++ ControleEstacionamento/FormPrincipal.cs
@@ -71,7 +71,7 @@ namespace ControleEstacionamento
 
         private void veiculosToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            new Relatorio.FormRelatorio().ShowDialog();
+            new Relatorio.FormRelatorioVeiculo().ShowDialog();
         }
     }
 }
---
+++ ControleEstacionamento/Relatorio/FormRelatorioVeiculoAvancado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleEstacionamento.Relatorio
+{
+    public partial class FormRelatorioVeiculoAvancado : Form
+    {
+        public FormRelatorioVeiculoAvancado()
+        {
+            InitializeComponent();
+        }
+    }
+}
