466ca025388eac44f840ee1363dab16c99b4e60e
tgspn <tiago.spana@gmail.com>
2017-06-11 19:24:16 -0300

    Relatórios

---
+++ ControleEstacionamento/Relatorio/FormRelatorioCliente.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace ControleEstacionamento.Relatorio
+{
+    partial class FormRelatorioCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnFiltrar = new System.Windows.Forms.Button();
+            this.gdVeiculo = new System.Windows.Forms.GroupBox();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.cmbMarca = new System.Windows.Forms.ComboBox();
+            this.lblCor = new System.Windows.Forms.Label();
+            this.cmbModelo = new System.Windows.Forms.ComboBox();
+            this.cmbCor = new System.Windows.Forms.ComboBox();
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblFinal = new System.Windows.Forms.Label();
+            this.lblInicial = new System.Windows.Forms.Label();
+            this.dtFinal = new System.Windows.Forms.DateTimePicker();
+            this.dtInicial = new System.Windows.Forms.DateTimePicker();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.panel1.SuspendLayout();
+            this.gdVeiculo.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnFiltrar);
+            this.panel1.Controls.Add(this.gdVeiculo);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(867, 96);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnFiltrar
+            // 
+            this.btnFiltrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFiltrar.Location = new System.Drawing.Point(762, 45);
+            this.btnFiltrar.Name = "btnFiltrar";
+            this.btnFiltrar.Size = new System.Drawing.Size(102, 23);
+            this.btnFiltrar.TabIndex = 8;
+            this.btnFiltrar.Text = "Filtrar";
+            this.btnFiltrar.UseVisualStyleBackColor = true;
+            this.btnFiltrar.Click += new System.EventHandler(this.btnFiltrar_Click);
+            // 
+            // gdVeiculo
+            // 
+            this.gdVeiculo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gdVeiculo.Controls.Add(this.lblMarca);
+            this.gdVeiculo.Controls.Add(this.cmbMarca);
+            this.gdVeiculo.Controls.Add(this.lblCor);
+            this.gdVeiculo.Controls.Add(this.cmbModelo);
+            this.gdVeiculo.Controls.Add(this.cmbCor);
+            this.gdVeiculo.Controls.Add(this.lblModelo);
+            this.gdVeiculo.Location = new System.Drawing.Point(12, 12);
+            this.gdVeiculo.Name = "gdVeiculo";
+            this.gdVeiculo.Size = new System.Drawing.Size(503, 67);
+            this.gdVeiculo.TabIndex = 7;
+            this.gdVeiculo.TabStop = false;
+            this.gdVeiculo.Text = "Veículo";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(18, 16);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 1;
+            this.lblMarca.Text = "Marca";
+            // 
+            // cmbMarca
+            // 
+            this.cmbMarca.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbMarca.FormattingEnabled = true;
+            this.cmbMarca.Location = new System.Drawing.Point(17, 35);
+            this.cmbMarca.Name = "cmbMarca";
+            this.cmbMarca.Size = new System.Drawing.Size(189, 21);
+            this.cmbMarca.TabIndex = 0;
+            // 
+            // lblCor
+            // 
+            this.lblCor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCor.AutoSize = true;
+            this.lblCor.Location = new System.Drawing.Point(408, 16);
+            this.lblCor.Name = "lblCor";
+            this.lblCor.Size = new System.Drawing.Size(23, 13);
+            this.lblCor.TabIndex = 5;
+            this.lblCor.Text = "Cor";
+            // 
+            // cmbModelo
+            // 
+            this.cmbModelo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbModelo.FormattingEnabled = true;
+            this.cmbModelo.Location = new System.Drawing.Point(212, 35);
+            this.cmbModelo.Name = "cmbModelo";
+            this.cmbModelo.Size = new System.Drawing.Size(189, 21);
+            this.cmbModelo.TabIndex = 2;
+            // 
+            // cmbCor
+            // 
+            this.cmbCor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbCor.FormattingEnabled = true;
+            this.cmbCor.Location = new System.Drawing.Point(408, 35);
+            this.cmbCor.Name = "cmbCor";
+            this.cmbCor.Size = new System.Drawing.Size(79, 21);
+            this.cmbCor.TabIndex = 4;
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Location = new System.Drawing.Point(213, 16);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(42, 13);
+            this.lblModelo.TabIndex = 3;
+            this.lblModelo.Text = "Modelo";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lblFinal);
+            this.groupBox1.Controls.Add(this.lblInicial);
+            this.groupBox1.Controls.Add(this.dtFinal);
+            this.groupBox1.Controls.Add(this.dtInicial);
+            this.groupBox1.Location = new System.Drawing.Point(521, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(235, 67);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ano de Fabricação";
+            // 
+            // lblFinal
+            // 
+            this.lblFinal.AutoSize = true;
+            this.lblFinal.Location = new System.Drawing.Point(124, 21);
+            this.lblFinal.Name = "lblFinal";
+            this.lblFinal.Size = new System.Drawing.Size(29, 13);
+            this.lblFinal.TabIndex = 3;
+            this.lblFinal.Text = "Final";
+            // 
+            // lblInicial
+            // 
+            this.lblInicial.AutoSize = true;
+            this.lblInicial.Location = new System.Drawing.Point(35, 21);
+            this.lblInicial.Name = "lblInicial";
+            this.lblInicial.Size = new System.Drawing.Size(34, 13);
+            this.lblInicial.TabIndex = 2;
+            this.lblInicial.Text = "Inicial";
+            // 
+            // dtFinal
+            // 
+            this.dtFinal.CustomFormat = "yyyy";
+            this.dtFinal.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtFinal.Location = new System.Drawing.Point(127, 36);
+            this.dtFinal.Name = "dtFinal";
+            this.dtFinal.ShowUpDown = true;
+            this.dtFinal.Size = new System.Drawing.Size(70, 20);
+            this.dtFinal.TabIndex = 1;
+            // 
+            // dtInicial
+            // 
+            this.dtInicial.CustomFormat = "yyyy";
+            this.dtInicial.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtInicial.Location = new System.Drawing.Point(38, 37);
+            this.dtInicial.Name = "dtInicial";
+            this.dtInicial.ShowUpDown = true;
+            this.dtInicial.Size = new System.Drawing.Size(70, 20);
+            this.dtInicial.TabIndex = 0;
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 96);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.ShowCloseButton = false;
+            this.crystalReportViewer1.ShowLogo = false;
+            this.crystalReportViewer1.Size = new System.Drawing.Size(867, 475);
+            this.crystalReportViewer1.TabIndex = 1;
+            this.crystalReportViewer1.ToolPanelView = CrystalDecisions.Windows.Forms.ToolPanelViewType.None;
+            // 
+            // FormRelatorioCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(867, 571);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormRelatorioCliente";
+            this.Text = "Relatório de clientes";
+            this.panel1.ResumeLayout(false);
+            this.gdVeiculo.ResumeLayout(false);
+            this.gdVeiculo.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblFinal;
+        private System.Windows.Forms.Label lblInicial;
+        private System.Windows.Forms.DateTimePicker dtFinal;
+        private System.Windows.Forms.DateTimePicker dtInicial;
+        private System.Windows.Forms.Label lblCor;
+        private System.Windows.Forms.ComboBox cmbCor;
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.ComboBox cmbModelo;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.ComboBox cmbMarca;
+        private System.Windows.Forms.Button btnFiltrar;
+        private System.Windows.Forms.GroupBox gdVeiculo;
+    }
+}
---
+++ ControleEstacionamento/Relatorio/FormRelatorioCliente.cs
@@ -0,0 +1,32 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleEstacionamento.Relatorio
+{
+    public partial class FormRelatorioCliente : Form
+    {
+        public FormRelatorioCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            var list = new List<Modelos.VeiculoModelo>(new Modelos.VeiculoModelo[] { new Modelos.VeiculoModelo() { Marca = "Chevrolet",Modelo="Corsa Classic",Ano="2004" }, new Modelos.VeiculoModelo { Marca = "Fiat", Modelo="Uno",Ano="2017" } });
+            // rptVeiculos1.SetDataSource(list);
+
+            ReportDocument rpt = new ReportDocument();
+            rpt.Load("Relatorio\\rptVeiculosAvancado.rpt");
+            rpt.SetDataSource(list);
+            crystalReportViewer1.ReportSource = rpt;
+        }
+    }
+}
