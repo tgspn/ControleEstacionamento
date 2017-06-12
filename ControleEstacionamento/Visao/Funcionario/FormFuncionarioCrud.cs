8b0650ea036bb886729bdd73028d44cdd403f5b2
tgspn <tiago.spana@gmail.com>
2017-06-11 22:55:14 -0300

    Ligação do controler com o form Funcionario

--- ControleEstacionamento/Visao/Funcionario/FormFuncionarioCrud.cs
+++ ControleEstacionamento/Visao/Funcionario/FormFuncionarioCrud.cs
@@ -16,36 +16,45 @@ namespace ControleEstacionamento.Visao.Funcionario
         public FormFuncionarioCrud()
         {
             InitializeComponent();
+            controler = new Controlers.FuncionarioController();
         }
-
-        public FuncionarioModelo modelo; //voltar privado depois do teste
+        public FormFuncionarioCrud(FuncionarioModelo modelo) : this()
+        {
+            this.Funcionario = modelo;
+        }
+        private Controlers.FuncionarioController controler;
+        public FuncionarioModelo Funcionario { get;private set; }
 
         public bool IsEdit { get; set; }
 
-        public FormFuncionarioCrud(FuncionarioModelo modelo) : this()
-        {
-            this.modelo = modelo;
-        }
+        
 
-        private void GetInfo()
+        private bool GetInfo()
         {
-            if (modelo == null)
+            if (Funcionario == null)
             {
-                modelo = new FuncionarioModelo();
+                Funcionario = new FuncionarioModelo();
             }
             if (Validar())
             {
-                modelo.Nome = txtNome.Text;
-                modelo.Cpf = txtCpf.Text;
-                modelo.Endereco = txtEndereco.Text;
-                modelo.Telefone = txtTelefone.Text;
-                modelo.Celular = txtCelular.Text;
-                modelo.Salario = decimal.Parse(txtSalario.Text);
+                Funcionario.Nome = txtNome.Text;
+                Funcionario.Cpf = txtCpf.Text;
+                Funcionario.Endereco = txtEndereco.Text;
+                Funcionario.Telefone = txtTelefone.Text;
+                Funcionario.Celular = txtCelular.Text;
+                Funcionario.Salario = decimal.Parse(txtSalario.Text);
+
+                if (IsEdit)
+                    controler.Atualizar(Funcionario);
+                else
+                    controler.Criar(Funcionario);
+
+                return true;
             }
             else
             {
                 MessageBox.Show("Existem campos obrigatórios não preenchidos!");
-                return;
+                return false;
             }
         }
 
@@ -85,20 +94,25 @@ namespace ControleEstacionamento.Visao.Funcionario
 
         private void btnSalvarFuncionario_Click(object sender, EventArgs e)
         {
-            GetInfo();
-            this.Close();
+            if(GetInfo())
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            
         }
 
         private void FormFuncionarioCrud_Load(object sender, EventArgs e)
         {
-            if (modelo != null)
+            if (Funcionario != null)
             {
-                SetInfo(modelo);
+                SetInfo(Funcionario);
             }
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
         {
+            this.DialogResult = DialogResult.Cancel;
             this.Close();
         }
     }
--- ControleEstacionamento/Visao/Funcionario/FormFuncionarioListar.cs
+++ ControleEstacionamento/Visao/Funcionario/FormFuncionarioListar.cs
@@ -13,20 +13,21 @@ namespace ControleEstacionamento.Visao.Funcionario
     public partial class FormFuncionarioListar : Form
     {
         //teste
-        BindingList<Modelos.FuncionarioModelo> list = new BindingList<Modelos.FuncionarioModelo>();
+        BindingList<Modelos.FuncionarioModelo> list;
+        Controlers.FuncionarioController controler;
 
         public FormFuncionarioListar()
         {
-            InitializeComponent();
-            dgvFuncionario.DataSource = list; //teste
+            InitializeComponent();            
         }
 
         private void btnAdicionar_Click(object sender, EventArgs e)
         {
-            FormFuncionarioCrud form = new FormFuncionarioCrud();
-            //form.MdiParent = this.MdiParent;
-            form.ShowDialog();
-            list.Add(form.modelo); //teste
+            FormFuncionarioCrud form = new FormFuncionarioCrud();            
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                list.Add(form.Funcionario); 
+            }
         }
 
         private void btnDetalhesVeiculo_Click(object sender, EventArgs e)
@@ -48,11 +49,24 @@ namespace ControleEstacionamento.Visao.Funcionario
 
         private void btnExcluirVeiculo_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Deseja excluir?", "Confirmação", MessageBoxButtons.YesNo);
+            var result=MessageBox.Show("Deseja excluir?", "Confirmação", MessageBoxButtons.YesNo);
+            if (result == DialogResult.OK)
+            {
+                var item= (Modelos.FuncionarioModelo)dgvFuncionario.SelectedRows[0].DataBoundItem;
+                if (controler.Excluir(item))
+                {
+                    list.Remove(item);
+                }   
+            }
         }
 
         private void btnFecharVeiculo_Click(object sender, EventArgs e) {
             this.Close();
+        }
+
+        private void FormFuncionarioListar_Load(object sender, EventArgs e)
+        {
+            dgvFuncionario.DataSource = list = new BindingList<Modelos.FuncionarioModelo>(controler.Listar());
         }
     }
 }
